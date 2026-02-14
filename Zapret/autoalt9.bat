fcd94690f2eed37fcc22128b50debaf6a154b17b
bludxdev <153301456+bludxdev@users.noreply.github.com>
2026-02-14 20:16:29 +0500

    Refactor autoalt9.bat to streamline service setup
    Removed installation steps for Windows Defender and driver, and updated service registration process.

--- Zapret/autoalt9.bat
+++ Zapret/autoalt9.bat
@@ -1,7 +1,9 @@
 @echo off
 setlocal EnableDelayedExpansion
+
+:: Проверка прав админа
 openfiles >nul 2>&1
-if %errorlevel% neq 0 (echo [!] Run as Admin! & pause & exit /b)
+if %errorlevel% neq 0 (exit /b)
 
 set "ZAPRET_ROOT=%~dp0.."
 set "BIN_PATH=%ZAPRET_ROOT%\bin\"
@@ -9,35 +11,20 @@ set "LISTS_PATH=%ZAPRET_ROOT%\lists\"
 set "WINWS=%BIN_PATH%winws.exe"
 set "SRVCNAME=zapret"
 
-:: 1. Add exclusion to Windows Defender
-echo [+] Adding folder to Windows Defender exclusions...
-powershell -Command "Add-MpPreference -ExclusionPath '%ZAPRET_ROOT%'"
+echo [+] Cleaning old service...
+sc stop %SRVCNAME% >nul 2>&1
+sc delete %SRVCNAME% >nul 2>&1
 
-:: 2. Pre-install driver
-echo [+] Installing WinDivert driver...
-cd /d "%BIN_PATH%"
-winws.exe --sys-install
-
-:: 3. Arguments
+:: Настройки для твоей версии v72.9
 set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-50100 ^
  --filter-udp=443 --hostlist="%LISTS_PATH%list-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN_PATH%quic_initial_www_google_com.bin" --new ^
  --filter-tcp=80 --hostlist="%LISTS_PATH%list-general.txt" --dpi-desync=fake80 --new ^
  --filter-tcp=443 --hostlist="%LISTS_PATH%list-general.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fake-tls="%BIN_PATH%tls_client_hello_www_google_com.bin"
 
-:: 4. Reinstall Service
-echo [+] Reinstalling service...
-sc stop %SRVCNAME% >nul 2>&1
-sc delete %SRVCNAME% >nul 2>&1
+echo [+] Registering service...
 sc create %SRVCNAME% binPath= "\"%WINWS%\" %ARGS%" DisplayName= "zapret" start= auto
+sc description %SRVCNAME% "Zapret DPI bypass"
 sc start %SRVCNAME%
 
-:: 5. Final check
-timeout /t 2
-sc query %SRVCNAME% | findstr RUNNING >nul
-if %errorlevel% equ 0 (
-    echo [OK] Service is RUNNING.
-) else (
-    echo [X] Service failed to start. Try running bin\winws.exe manually to see error.
-)
-timeout /t 5
+echo [+] Done.
 exit
