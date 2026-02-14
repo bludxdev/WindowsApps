77050b78f1130e53ed9cc56ead3f25d81d1fe852
bludxdev <153301456+bludxdev@users.noreply.github.com>
2026-02-14 20:21:40 +0500

    Update service creation script for zapret

--- Zapret/autoalt9.bat
+++ Zapret/autoalt9.bat
@@ -1,15 +1,12 @@
 @echo off
-set "BIN_DIR=%~dp0..\bin"
-set "LISTS=%~dp0..\lists"
+set "Z_BIN=%~dp0..\bin\winws.exe"
+set "Z_LIST=%~dp0..\lists\list-general.txt"
 
-:: Останавливаем и удаляем старое
 sc stop zapret >nul 2>&1
 sc delete zapret >nul 2>&1
 
-:: Создаем сервис с чистыми флагами (БЕЗ --sys-install)
-:: Важно: после binPath= ОБЯЗАТЕЛЬНО должен быть пробел
-sc create zapret binPath= "\"%BIN_DIR%\winws.exe\" --wf-tcp=80,443 --wf-udp=443 --filter-udp=443 --hostlist=\"%LISTS%\list-general.txt\" --dpi-desync=fake --new --filter-tcp=443 --hostlist=\"%LISTS%\list-general.txt\" --dpi-desync=fake,split2" DisplayName= "zapret" start= auto
+:: Регистрируем без лишних наворотов
+sc create zapret binPath= "\"%Z_BIN%\" --wf-tcp=80,443 --wf-udp=443 --filter-udp=443 --hostlist=\"%Z_LIST%\" --dpi-desync=fake --new --filter-tcp=443 --hostlist=\"%Z_LIST%\" --dpi-desync=fake,split2" DisplayName= "zapret" start= auto
 
-:: Запускаем
 sc start zapret
 exit
